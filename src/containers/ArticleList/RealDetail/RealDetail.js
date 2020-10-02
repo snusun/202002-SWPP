a08767076495861d6849ab37e55904ba3ffc17dc
snusun <0515hwang@snu.ac.kr>
2020-10-02 19:20:19 +0900

    develop back button

--- src/containers/ArticleList/RealDetail/RealDetail.js
+++ src/containers/ArticleList/RealDetail/RealDetail.js
@@ -9,6 +9,10 @@ class RealDetail extends Component {
         this.props.onGetArticle(parseInt(this.props.match.params.id));
     }
 
+    handleBackButton = () => {
+        this.props.history.push('/articles');
+    }
+
     render() {
         let title='', content='', author_id=0;
         if(this.props.selectedArticle) { //null check
@@ -19,32 +23,29 @@ class RealDetail extends Component {
 
         return (
             <div className="TodoDetail">
-                <div className="row">
-                    <div className="left">
-                        Title: 
-                    </div>
-                    <div className="right">
-                        {title}
-                    </div>
+                <div>
+                   <p id='article-title'>Title: {title}</p>
                 </div>
-                <div className="row">
-                    <div className="left">
-                        Content: 
-                    </div>
-                    <div className="right">
-                       {content}
-                    </div>
+                <div>
+                    <p id='article-content'>Content: {content}</p>
                 </div>
-                <div className="row">
-                    <div className="left">
-                        AuthorID: 
-                    </div>
-                    <div className="right">
-                        {author_id}
-                    </div>
-                    <button  onClick={ () => this.handleDeleteButton() }>delete</button> 
-                    
+                <div>
+                    <p id='article-author'>AuthorID: {author_id}</p>              
                 </div>
+                <div>
+                    <button id='edit-article-button'>edit</button>
+                    <button id='delete-article-button' onClick={ () => this.handleDeleteButton() }>delete</button> 
+                    <button id='back-detail-article-button' onClick={ () => this.handleBackButton() }>back</button>
+                </div>
+                <div>
+                    <textarea id='new-comment-content-input' type="text"/>
+                </div>
+                <div>
+                    <button id='confirm-create-comment-button'>create</button>
+                    <button id='edit-comment-button'>edit</button> 
+                    <button id='delete-comment-button'>delete</button>
+                </div>
+
             </div>
         );
     }
