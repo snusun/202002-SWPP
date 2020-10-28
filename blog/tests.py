bd6afedbd46ba8bf367e18f9f14967d7cac121c4
snusun <0515hwang@snu.ac.kr>
2020-10-29 01:53:38 +0900

    coverage 95%

--- blog/tests.py
+++ blog/tests.py
@@ -4,10 +4,6 @@ from .models import Article, Comment
 import json
 
 class BlogTestCase(TestCase):
-    def setUp(self):
-        User.objects.create_user(
-            username="test", email="testemail", password="testpassword")
-
     def test_csrf(self):
         # By default, csrf checks are disabled in test client
         # To test csrf protection we enforce csrf checks here
@@ -112,3 +108,109 @@ class BlogTestCase(TestCase):
         response = client.post('/api/comment/1/')
         self.assertEqual(response.status_code, 405)
 
+    def test_articleAndComment(self):
+        client = Client()
+
+        response = client.post('/api/signup/', json.dumps({'username': 'test1', 'password': 'testpassword'}),
+                                content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        response = client.post('/api/signup/', json.dumps({'username': 'test2', 'password': 'testpassword'}),
+                                content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        response = client.post('/api/signin/', json.dumps({'username': 'test1', 'password': 'testpassword'}),
+                                content_type='application/json')
+        self.assertEqual(response.status_code, 204)
+
+        #create new article
+        response = client.post('/api/article/', json.dumps({'title': 'testArticle1', 'content': 'testContent1'}),
+                                content_type='application/json')
+        response = client.post('/api/article/', json.dumps({'title': 'testArticle2', 'content': 'testContent2'}),
+                                content_type='application/json')
+        response = client.post('/api/article/', json.dumps({'title': 'testArticle3', 'content': 'testContent3'}),
+                                content_type='application/json')
+        self.assertIn('1', response.content.decode())
+        self.assertEqual(response.status_code, 201)
+
+        #get article list
+        response = client.get('/api/article/')
+        article_all_list = json.loads(response.content.decode())
+        self.assertEqual(len(article_all_list), 3)
+
+        #get specified article
+        response = client.get('/api/article/1/')
+        article = json.loads(response.content)
+        self.assertEqual(article['title'], 'testArticle1')
+        
+        #get wrong article
+        response = client.get('/api/article/5/')
+        self.assertEqual(response.status_code, 404)
+
+        #edit specified article
+        response = client.put('/api/article/1/', json.dumps({'title': 'test', 'content': 'testContent1'}),
+                                content_type='application/json')
+        article = json.loads(response.content)
+        self.assertEqual(article['title'], 'test')
+        self.assertEqual(response.status_code, 200)
+
+        #edit wrong article
+        response = client.put('/api/article/5/')
+        self.assertEqual(response.status_code, 404)
+        
+        #delete specified article
+        response = client.delete('/api/article/2/')
+        self.assertEqual(response.status_code, 200)
+
+        #delete wrong article
+        response = client.delete('/api/article/5/')
+        self.assertEqual(response.status_code, 404)
+
+        #create new comment
+        response = client.post('/api/article/1/comment/', json.dumps({'content': 'test1'}),
+                                content_type='application/json')
+        response = client.post('/api/article/1/comment/', json.dumps({'content': 'test2'}),
+                                content_type='application/json')
+        self.assertIn('', response.content.decode())
+        self.assertEqual(response.status_code, 201)
+
+        response = client.post('/api/article/2/comment/', json.dumps({'content': 'test1'}),
+                                content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+
+        #get comments of specified article
+        response = client.get('/api/article/1/comment/')
+        comments = json.loads(response.content.decode())
+        self.assertEqual(len(comments), 2)
+
+        #get comments of wrong article
+        response = client.get('/api/article/2/comment/')
+        self.assertEqual(response.status_code, 404)
+
+        #get specified comment
+        response = client.get('/api/comment/1/')
+        article = json.loads(response.content)
+        self.assertEqual(article['content'], 'test1')
+
+        #get wrong comment
+        response = client.get('/api/comment/5/')
+        self.assertEqual(response.status_code, 404)
+
+        #edit specified comment
+        response = client.put('/api/comment/1/', json.dumps({'content': 'testContent1'}),
+                                content_type='application/json')
+        comment = json.loads(response.content)
+        self.assertEqual(comment['content'], 'testContent1')
+        self.assertEqual(response.status_code, 200)
+
+        #edit wrong comment
+        response = client.put('/api/comment/5/')
+        self.assertEqual(response.status_code, 404)
+        
+        #delete specified comment
+        response = client.delete('/api/comment/2/')
+        self.assertEqual(response.status_code, 200)
+
+        #delete wrong comment
+        response = client.delete('/api/comment/5/')
+        self.assertEqual(response.status_code, 404)
